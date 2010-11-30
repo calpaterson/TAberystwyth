8be04df2e7ddcd6a5e7b6ed3b657cddf2b8b9b32
r <r@sarrionandia.com>
2010-11-30 17:00:21 +0000

    Insert Menu
    Ignore-this: 5323c8a24f2424cbcf23c01793ab5804
    
    darcs-hash:20101130170021-2a117-e9cf7ce88621b4ec3d6e777af2db1e8c8d3be0a6.gz

--- src/taberystwyth/view/MainFrameMenu.java
+++ src/taberystwyth/view/MainFrameMenu.java
@@ -7,10 +7,15 @@ import javax.swing.JMenuItem;
 public class MainFrameMenu extends JMenuBar {
 	JMenu fileMenu = new JMenu("File");
 	JMenu roundsMenu = new JMenu("Rounds");
+	JMenu insertMenu = new JMenu("Insert");
 	
 	JMenuItem open = new JMenuItem("Open");
 	JMenuItem save = new JMenuItem("Save");
 	JMenuItem quit = new JMenuItem("Quit");
+	
+	JMenuItem speakers = new JMenuItem("Speakers");
+	JMenuItem judges = new JMenuItem("Judges");
+	JMenuItem locations = new JMenuItem("Locations");
 	
 	JMenuItem drawRound = new JMenuItem("Draw Round");
 	JMenuItem viewRounds = new JMenuItem("View Rounds");
@@ -20,10 +25,15 @@ public class MainFrameMenu extends JMenuBar {
 		fileMenu.add(save);
 		fileMenu.add(quit);
 		
+		insertMenu.add(speakers);
+		insertMenu.add(judges);
+		insertMenu.add(locations);
+		
 		roundsMenu.add(drawRound);
 		roundsMenu.add(viewRounds);
 		
 		add(fileMenu);
+		add(insertMenu);
 		add(roundsMenu);
 	}
 	

90de211c171900450ed8647fe8358d32f7b0a48c
clp8 <clp8@aber.ac.uk>
2010-11-30 16:50:00 +0000

    OverviewFrame skeleton
    Ignore-this: f2c6aba5b93d6b94f5bc5681902c85fe
    
    darcs-hash:20101130165000-aaae6-0b259b765614e3c26c3025ac0775d89de3e1876b.gz

--- src/taberystwyth/view/OverviewFrame.java
+++ src/taberystwyth/view/OverviewFrame.java
@@ -1,13 +1,22 @@
 package taberystwyth.view;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.*;
 
 public class OverviewFrame extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+
 	public OverviewFrame(){
 		setLayout(new BorderLayout());
-		add(new JMenuBar(), BorderLayout.NORTH);
+		add(new MainFrameMenu(), BorderLayout.NORTH);
+		JPanel viewPanel = new JPanel(new GridLayout(1,3));
+		viewPanel.add(new JLabel("Judges"));
+		viewPanel.add(new JLabel("Teams"));
+		viewPanel.add(new JLabel("Speakers"));
+		add(viewPanel);
 		setVisible(true);
 		pack();
 	}

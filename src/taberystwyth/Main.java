eaf49d8408128e71dc3e7686f5b17fb79189b486
clp8 <clp8@aber.ac.uk>
2010-11-30 16:49:34 +0000

    Readme and one small Main correction
    Ignore-this: c5f1921d54feb794af92558374de6303
    
    darcs-hash:20101130164934-aaae6-87ba210f0dd660278f7591e461bc4a653937b926.gz

--- src/taberystwyth/Main.java
+++ src/taberystwyth/Main.java
@@ -1,5 +1,9 @@
 package taberystwyth;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.swing.*;
 
 import taberystwyth.view.OverviewFrame;

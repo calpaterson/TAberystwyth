6871307b68dbd67378853bb21c674e83f53cd9f0
r <r@sarrionandia.com>
2010-11-30 16:50:08 +0000

    Menu Structure
    Ignore-this: 6fcd325ab3446615a5923b8305fd1ea7
    
    darcs-hash:20101130165008-2a117-335e25d456af2c1a389f3d0bbae5050418bdba6f.gz

--- src/taberystwyth/view/MainFrameMenu.java
+++ src/taberystwyth/view/MainFrameMenu.java
@@ -1,7 +1,27 @@
 package taberystwyth.view;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MainFrameMenu extends JMenuBar {
-
+	JMenu fileMenu = new JMenu("File");
+	JMenu roundsMenu = new JMenu("Rounds");
+	
+	JMenuItem open = new JMenuItem("Open");
+	JMenuItem save = new JMenuItem("Save");
+	JMenuItem quit = new JMenuItem("Quit");
+	
+	JMenuItem drawRound = new JMenuItem("Draw Round");
+	JMenuItem viewRounds = new JMenuItem("View Rounds");
+	
+	public MainFrameMenu(){
+		fileMenu.add(open);
+		fileMenu.add(save);
+		fileMenu.add(quit);
+		
+		roundsMenu.add(drawRound);
+		roundsMenu.add(viewRounds);
+	}
+	
 }
--- src/taberystwyth/view/MainFrameMenuBar.java
+++ src/taberystwyth/view/MainFrameMenuBar.java
@@ -3,5 +3,11 @@ package taberystwyth.view;
 import javax.swing.JMenuBar;
 
 public class MainFrameMenuBar extends JMenuBar {
+	MainFrameMenu menu;
+	MainFrameMenuBar(){
+		setVisible(true);
+		this.add(menu);
+		
+	}
 
 }

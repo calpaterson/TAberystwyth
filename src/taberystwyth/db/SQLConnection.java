34ed631d53b75dfada05304465cc3602175779ca
clp8 <clp8@aber.ac.uk>
2010-12-01 18:14:25 +0000

    Comments in SQLConnection
    Ignore-this: e933137afbb329546b869c48ddf116e3
    
    darcs-hash:20101201181425-aaae6-d051852bd6c84f029f8f5aeab744bcc7009acce3.gz

--- src/taberystwyth/db/SQLConnection.java
+++ src/taberystwyth/db/SQLConnection.java
@@ -7,79 +7,87 @@ import java.util.*;
 import javax.swing.JOptionPane;
 
 public class SQLConnection {
-	
+
 	private static SQLConnection instance = new SQLConnection();
-	public static SQLConnection getInstance(){
+
+	public static SQLConnection getInstance() {
 		return instance;
 	}
-	
+
 	Connection conn;
 	String SQLError = "There was some kind of problem accessing the database.";
-	
-	private SQLConnection(){
+
+	private SQLConnection() {
 		try {
 			/*
 			 * Ensure that the SQL driver is pulled through the class loader
 			 */
 			Class.forName("org.sqlite.JDBC");
-			
+
 			/*
 			 * Load the database FIXME: this needs to be way more flexible
 			 */
 			conn = DriverManager.getConnection("jdbc:sqlite:taberystwyth.tab");
-			
+
 			/*
 			 * If the tables don't already exist, load them.
 			 */
-			HashSet<String> expected = new HashSet<String>(){
+			HashSet<String> expected = new HashSet<String>() {
 				private static final long serialVersionUID = 1L;
 				{
-				add("LOCATION");
-				add("PANEL");
-				add("RESULTS");
-				add("ROOM");
-				add("SPEAKER");
-				add("SPEAKER_POINTS");
-				add("TEAM");
-				add("SQLITE_AUTOINDEX_PANEL_1"); // automatic
-			}};
+					add("LOCATION");
+					add("PANEL");
+					add("RESULTS");
+					add("ROOM");
+					add("SPEAKER");
+					add("SPEAKER_POINTS");
+					add("TEAM");
+					add("SQLITE_AUTOINDEX_PANEL_1"); // automatic
+				}};
 			HashSet<String> actual = new HashSet<String>();
-			
+
 			/*
-			 * Fill up the actual
+			 * See what tables we currently have
 			 */
 			ResultSet rs = conn.getMetaData().getTables(null, null, null, null);
-			while (rs.next()){
-			    actual.add(rs.getString("TABLE_NAME"));
+			while (rs.next()) {
+				actual.add(rs.getString("TABLE_NAME"));
 			}
-			
+
 			/*
-			 * If not equal then load the sql files
+			 * If we don't have the tables, we need to create them, using data/* .
 			 */
-			if (!actual.equals(expected)){
-				String[] sqlFiles = {"location", "panel", "results", "room", "speaker", "speaker_points",
-						"team"};
-				for (String s: sqlFiles){
-					System.out.println("SQLConnection: evaluating " + s + ".sql");
+			if (!actual.equals(expected)) {
+				String[] sqlFiles = { "location", "panel", "results", "room",
+						"speaker", "speaker_points", "team" };
+				for (String s : sqlFiles) {
+					System.out.println("SQLConnection: evaluating " + s
+							+ ".sql");
 					evaluateSQLFile("data/" + s + ".sql");
 				}
 			}
-			
+
 			rs.close();
-			} catch (Exception e) {
+		} catch (Exception e) {
+			/*
+			 * If anything goes wrong anywhere in the constructor, it's pretty safe to say that the 
+			 * whole program cannot continue.
+			 */
 			String error = "There was some kind of SQL initialisation problem and I cannot continue.";
 			JOptionPane.showMessageDialog(null, error);
 			e.printStackTrace();
 			System.exit(255);
 		}
 	}
-	
+
 	/**
 	 * Execute an SQL query against the database
-	 * @param query query
+	 * 
+	 * @param query
+	 *            query
 	 * @return resultset
 	 */
-	public synchronized ResultSet executeQuery(String query){
+	public synchronized ResultSet executeQuery(String query) {
 		ResultSet returnValue = null;
 		try {
 			Statement stmt = conn.createStatement();
@@ -90,13 +98,15 @@ public class SQLConnection {
 		}
 		return returnValue;
 	}
-	
+
 	/**
 	 * Execute an SQL statement against the database
-	 * @param statement statement
+	 * 
+	 * @param statement
+	 *            statement
 	 * @return success or failure
 	 */
-	public synchronized boolean execute(String statement){
+	public synchronized boolean execute(String statement) {
 		boolean returnValue = false;
 		try {
 			Statement stmt = conn.createStatement();
@@ -107,19 +117,25 @@ public class SQLConnection {
 		}
 		return returnValue;
 	}
-	
-    /**
-     * Evaluates a given SQL file against the given connection.
-     * @param conn connection to SQL database
-     * @param filePath path of SQL file
-     * @throws IOException if the file is  not found
-     * @throws SQLException if there is some problem with the SQL server
-     */
-    private synchronized void evaluateSQLFile(String filePath) throws IOException, SQLException {
-        char[] cbuf = new char[2000];
-        new BufferedReader(new FileReader(new File(filePath))).read(cbuf);
-        //System.out.println(new String(cbuf));
-        conn.createStatement().execute(new String(cbuf));
-    }
+
+	/**
+	 * Evaluates a given SQL file against the given connection.
+	 * 
+	 * @param conn
+	 *            connection to SQL database
+	 * @param filePath
+	 *            path of SQL file
+	 * @throws IOException
+	 *             if the file is not found
+	 * @throws SQLException
+	 *             if there is some problem with the SQL server
+	 */
+	private synchronized void evaluateSQLFile(String filePath)
+			throws IOException, SQLException {
+		char[] cbuf = new char[2000];
+		new BufferedReader(new FileReader(new File(filePath))).read(cbuf);
+		// System.out.println(new String(cbuf));
+		conn.createStatement().execute(new String(cbuf));
+	}
 
 }

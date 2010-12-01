0427d60e4fbab4127dc7194ae0f1257732494d6d
clp8 <clp8@aber.ac.uk>
2010-12-01 21:47:52 +0000

    Much improved error messages and handling in SQLConnection
    Ignore-this: f43b948076bb697c6fdcaad42c838f63
    
    darcs-hash:20101201214752-aaae6-86522622a592531cb5fc0c9bb64c2fc6b0a3d390.gz

--- src/taberystwyth/db/SQLConnection.java
+++ src/taberystwyth/db/SQLConnection.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 import javax.swing.JOptionPane;
 
+import taberystwyth.view.OverviewFrame;
+
 public class SQLConnection {
 
 	private static SQLConnection instance = new SQLConnection();
@@ -17,17 +19,29 @@ public class SQLConnection {
 	Connection conn;
 	String SQLError = "There was some kind of problem accessing the database.";
 
-	private SQLConnection() {
+	public void setDatabase(String filePath){
 		try {
+			conn = DriverManager.getConnection("jdbc:sqlite:" + filePath);
+		} catch (SQLException e) {
+			panic(e, "Unable to load the following file:\n" + filePath);
+		}
+	}
+	
+	private SQLConnection() {
 			/*
 			 * Ensure that the SQL driver is pulled through the class loader
 			 */
-			Class.forName("org.sqlite.JDBC");
+			try {
+				Class.forName("org.sqlite.JDBC");
+			} catch (ClassNotFoundException e) {
+				panic(e, "Unable to load the database driver."+
+						"Perhaps your computer architecture is not supported?");
+			}
 
 			/*
 			 * Load the database FIXME: this needs to be way more flexible
 			 */
-			conn = DriverManager.getConnection("jdbc:sqlite:taberystwyth.tab");
+			setDatabase("taberystwyth.tab");
 
 			/*
 			 * If the tables don't already exist, load them.
@@ -49,9 +63,14 @@ public class SQLConnection {
 			/*
 			 * See what tables we currently have
 			 */
-			ResultSet rs = conn.getMetaData().getTables(null, null, null, null);
+			ResultSet rs = null;
+			try{
+			rs = conn.getMetaData().getTables(null, null, null, null);
 			while (rs.next()) {
 				actual.add(rs.getString("TABLE_NAME"));
+			}
+			} catch (SQLException e){
+				panic(e, "Unable to check what tables are already in the database");
 			}
 
 			/*
@@ -67,17 +86,25 @@ public class SQLConnection {
 				}
 			}
 
-			rs.close();
-		} catch (Exception e) {
-			/*
-			 * If anything goes wrong anywhere in the constructor, it's pretty safe to say that the 
-			 * whole program cannot continue.
-			 */
-			String error = "There was some kind of SQL initialisation problem and I cannot continue.";
-			JOptionPane.showMessageDialog(null, error);
-			e.printStackTrace();
-			System.exit(255);
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				panic(e, "Unable to close a resultset.");
+			}
+	}
+	
+	/**
+	 * There are so many instances where an unfixable situation arises in this file that I have defined
+	 * this shorthand.
+	 * @param e an exception
+	 * @param reason a Very Informative Description of the problem
+	 */
+	private void panic(Exception e, String reason){
+		JOptionPane.showMessageDialog(OverviewFrame.getInstance(), reason);
+		if (e != null){
+		e.printStackTrace();
 		}
+		System.exit(255);
 	}
 
 	/**
@@ -93,8 +120,7 @@ public class SQLConnection {
 			Statement stmt = conn.createStatement();
 			returnValue = stmt.executeQuery(query);
 		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, SQLError);
-			e.printStackTrace();
+			panic(e, "Unable to execute this query against the database:\n" + query);
 		}
 		return returnValue;
 	}
@@ -112,8 +138,7 @@ public class SQLConnection {
 			Statement stmt = conn.createStatement();
 			returnValue = stmt.execute(statement);
 		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, SQLError);
-			e.printStackTrace();
+			panic(e, "Unable to execute this statement against the database:\n" + statement);
 		}
 		return returnValue;
 	}
@@ -130,12 +155,14 @@ public class SQLConnection {
 	 * @throws SQLException
 	 *             if there is some problem with the SQL server
 	 */
-	private synchronized void evaluateSQLFile(String filePath)
-			throws IOException, SQLException {
+	private synchronized void evaluateSQLFile(String filePath){
 		char[] cbuf = new char[2000];
-		new BufferedReader(new FileReader(new File(filePath))).read(cbuf);
-		// System.out.println(new String(cbuf));
-		conn.createStatement().execute(new String(cbuf));
+		try {
+			new BufferedReader(new FileReader(new File(filePath))).read(cbuf);
+			conn.createStatement().execute(new String(cbuf));
+		} catch (Exception e) {
+			panic(e, "Unable to evaluate this file:\n" + new File(filePath).getAbsolutePath());
+		}
 	}
 
 }

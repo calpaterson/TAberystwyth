5ca6e106990836c817b56c857d06e3102e9a2fb0
clp8 <clp8@aber.ac.uk>
2010-12-01 18:54:48 +0000

    m
    Ignore-this: f4bc4f8c857cf896db29982266a672ea
    
    darcs-hash:20101201185448-aaae6-7f486d6dc301f44eed8f28f063963c276013b1d7.gz

--- src/taberystwyth/Main.java
+++ src/taberystwyth/Main.java
@@ -1,14 +1,14 @@
 package taberystwyth;
 
 import javax.swing.*;
-
-import taberystwyth.db.SQLConnection;
+import taberystwyth.db.*;
 import taberystwyth.view.OverviewFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
+			Class.forName("taberystwyth.db.SQLConnection");
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e){
 			e.printStackTrace();

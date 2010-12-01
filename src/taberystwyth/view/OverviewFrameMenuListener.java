6a969ba9b5a9e55467ca9962f49773382f04c1b5
r <r@sarrionandia.com>
2010-12-01 16:05:11 +0000

    OverviewFrameMenuListener added
    Ignore-this: 407a474d538705d568ea552e3444f430
    
    darcs-hash:20101201160511-2a117-fb98d747b5c196586dfb40f52401918e1051dace.gz

---
+++ src/taberystwyth/view/OverviewFrameMenuListener.java
@@ -0,0 +1,13 @@
+package taberystwyth.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class OverviewFrameMenuListener implements ActionListener {
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

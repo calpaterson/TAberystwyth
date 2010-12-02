c3c9ba9f386888fc21ba79d62dc5059a56c0edcd
clp8 <clp8@aber.ac.uk>
2010-12-02 12:56:29 +0000

    LocationInsertionFrame has buttons
    Ignore-this: 117339d8729117a35ebd0e5561f8d5db
    
    darcs-hash:20101202125629-aaae6-979c2d7872f934119fe4bc7426af7833c95c8db1.gz

--- src/taberystwyth/view/LocationInsertionFrame.java
+++ src/taberystwyth/view/LocationInsertionFrame.java
@@ -10,20 +10,32 @@ public class LocationInsertionFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	SQLConnection conn = SQLConnection.getInstance();
 	
+	/*
+	 * Textfields
+	 */
 	JTextField name = new JTextField();
 	JLabel nameLabel = new JLabel("Name:");
 	JTextField rating = new JTextField();
 	JLabel ratingLabel = new JLabel("Rating:");
 	
+	/*
+	 * Buttons
+	 */
+	JButton clear = new JButton("Clear");
+	JButton save = new JButton("Save");
+	
 	public LocationInsertionFrame(){
 		setVisible(true);
-		setLayout(new GridLayout(2,2));
+		setLayout(new GridLayout(3,2));
 		setTitle("Insert Location");
 		
 		add(nameLabel);
 		add(name);
 		add(ratingLabel);
 		add(rating);
+		
+		add(clear);
+		add(save);
 		
 		pack();
 	}

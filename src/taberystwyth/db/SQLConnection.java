c2d2b1c6535044a641a0d9a6068f747a070d2327
clp8 <clp8@aber.ac.uk>
2010-12-02 01:22:20 +0000

    SQLConnection cleaned up
    Ignore-this: 9ca80ba939d021b69bdbea733caf3732
    
    darcs-hash:20101202012220-aaae6-2e8a1abdb47e1a82f4660bdbf2deeaf2ddeb272b.gz

--- src/taberystwyth/db/SQLConnection.java
+++ src/taberystwyth/db/SQLConnection.java
@@ -16,113 +16,113 @@ public class SQLConnection {
 		return instance;
 	}
 
-	Connection conn;
-	String SQLError = "There was some kind of problem accessing the database.";
+	private Connection conn;
 
-	public void setDatabase(File file){
-		try {
-			conn = DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
-		} catch (SQLException e) {
-			panic(e, "Unable to load the following file:\n" + file);
-		}
-	}
-	
 	private SQLConnection() {
-			/*
-			 * Ensure that the SQL driver is pulled through the class loader
-			 */
-			try {
-				Class.forName("org.sqlite.JDBC");
-			} catch (ClassNotFoundException e) {
-				panic(e, "Unable to load the database driver."+
-						"Perhaps your computer architecture is not supported?");
+		/*
+		 * Ensure that the SQL driver is pulled through the class loader
+		 */
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			panic(e, "Unable to load the database driver."
+					+ "Perhaps your computer architecture is not supported?");
+		}
+
+		/*
+		 * Load the database FIXME: the user should be consulted about which one
+		 */
+		setDatabase(new File("taberystwyth.tab"));
+
+		/*
+		 * If the tables don't already exist, load them.
+		 */
+		@SuppressWarnings("serial")
+		HashSet<String> expected = new HashSet<String>() {
+			{
+				add("LOCATION");
+				add("PANEL");
+				add("RESULTS");
+				add("ROOM");
+				add("SPEAKER");
+				add("SPEAKER_POINTS");
+				add("TEAM");
+				add("SQLITE_AUTOINDEX_PANEL_1"); // automatic
 			}
+		};
+		HashSet<String> actual = new HashSet<String>();
 
-			/*
-			 * Load the database FIXME: the user should be consulted about which one
-			 */
-			setDatabase(new File("taberystwyth.tab"));
-
-			/*
-			 * If the tables don't already exist, load them.
-			 */
-			@SuppressWarnings("serial")
-			HashSet<String> expected = new HashSet<String>() {
-				{
-					add("LOCATION");
-					add("PANEL");
-					add("RESULTS");
-					add("ROOM");
-					add("SPEAKER");
-					add("SPEAKER_POINTS");
-					add("TEAM");
-					add("SQLITE_AUTOINDEX_PANEL_1"); // automatic
-				}};
-			HashSet<String> actual = new HashSet<String>();
-
-			/*
-			 * See what tables we currently have
-			 */
-			ResultSet rs = null;
-			try{
+		/*
+		 * See what tables we currently have
+		 */
+		ResultSet rs = null;
+		try {
 			rs = conn.getMetaData().getTables(null, null, null, null);
 			while (rs.next()) {
 				actual.add(rs.getString("TABLE_NAME"));
 			}
-			} catch (SQLException e){
-				panic(e, "Unable to check what tables are already in the database");
-			}
+		} catch (SQLException e) {
+			panic(e, "Unable to check what tables are already in the database");
+		}
 
-			/*
-			 * If we don't have the tables, we need to create them, using data/* .
-			 */
-			if (!actual.equals(expected)) {
-				String[] sqlFiles = { "location", "panel", "results", "room",
-						"speaker", "speaker_points", "team" };
-				for (String s : sqlFiles) {
-					System.out.println("SQLConnection: evaluating " + s
-							+ ".sql");
-					evaluateSQLFile("data/" + s + ".sql");
-				}
+		/*
+		 * If we don't have the tables, we need to create them, using data/* .
+		 */
+		if (!actual.equals(expected)) {
+			String[] sqlFiles = { "location", "panel", "results", "room",
+					"speaker", "speaker_points", "team" };
+			for (String s : sqlFiles) {
+				System.out.println("SQLConnection: evaluating " + s + ".sql");
+				evaluateSQLFile("data/" + s + ".sql");
 			}
+		}
 
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				panic(e, "Unable to close a resultset.");
-			}
+		try {
+			rs.close();
+		} catch (SQLException e) {
+			panic(e, "Unable to close a resultset.");
+		}
 	}
-	
+
 	/**
-	 * There are so many instances where an unfixable situation arises in this file that I have defined
-	 * this shorthand.
-	 * @param e an exception
-	 * @param reason a Very Informative Description of the problem
+	 * Evaluates a given SQL file against the given connection.
+	 * 
+	 * @param conn
+	 *            connection to SQL database
+	 * @param filePath
+	 *            path of SQL file
+	 * @throws IOException
+	 *             if the file is not found
+	 * @throws SQLException
+	 *             if there is some problem with the SQL server
 	 */
-	private void panic(Exception e, String reason){
+	private synchronized void evaluateSQLFile(String filePath) {
+		char[] cbuf = new char[2000];
+		try {
+			new BufferedReader(new FileReader(new File(filePath))).read(cbuf);
+			conn.createStatement().execute(new String(cbuf));
+		} catch (Exception e) {
+			panic(e,
+					"Unable to evaluate this file:\n"
+							+ new File(filePath).getAbsolutePath());
+		}
+	}
+
+	/**
+	 * There are so many instances where an unfixable situation arises in this
+	 * file that I have defined this shorthand.
+	 * 
+	 * @param e
+	 *            an exception
+	 * @param reason
+	 *            a Very Informative Description of the problem
+	 */
+	private void panic(Exception e, String reason) {
 		JOptionPane.showMessageDialog(OverviewFrame.getInstance(), reason);
-		if (e != null){
-		e.printStackTrace();
+		if (e != null) {
+			e.printStackTrace();
 		}
 		System.exit(255);
-	}
-
-	/**
-	 * Execute an SQL query against the database
-	 * 
-	 * @param query
-	 *            query
-	 * @return resultset
-	 */
-	public synchronized ResultSet executeQuery(String query) {
-		ResultSet returnValue = null;
-		try {
-			Statement stmt = conn.createStatement();
-			returnValue = stmt.executeQuery(query);
-		} catch (SQLException e) {
-			panic(e, "Unable to execute this query against the database:\n" + query);
-		}
-		return returnValue;
 	}
 
 	/**
@@ -138,30 +138,37 @@ public class SQLConnection {
 			Statement stmt = conn.createStatement();
 			returnValue = stmt.execute(statement);
 		} catch (SQLException e) {
-			panic(e, "Unable to execute this statement against the database:\n" + statement);
+			panic(e, "Unable to execute this statement against the database:\n"
+					+ statement);
 		}
 		return returnValue;
 	}
 
 	/**
-	 * Evaluates a given SQL file against the given connection.
+	 * Execute an SQL query against the database
 	 * 
-	 * @param conn
-	 *            connection to SQL database
-	 * @param filePath
-	 *            path of SQL file
-	 * @throws IOException
-	 *             if the file is not found
-	 * @throws SQLException
-	 *             if there is some problem with the SQL server
+	 * @param query
+	 *            query
+	 * @return resultset
 	 */
-	private synchronized void evaluateSQLFile(String filePath){
-		char[] cbuf = new char[2000];
+	public synchronized ResultSet executeQuery(String query) {
+		ResultSet returnValue = null;
 		try {
-			new BufferedReader(new FileReader(new File(filePath))).read(cbuf);
-			conn.createStatement().execute(new String(cbuf));
-		} catch (Exception e) {
-			panic(e, "Unable to evaluate this file:\n" + new File(filePath).getAbsolutePath());
+			Statement stmt = conn.createStatement();
+			returnValue = stmt.executeQuery(query);
+		} catch (SQLException e) {
+			panic(e, "Unable to execute this query against the database:\n"
+					+ query);
+		}
+		return returnValue;
+	}
+
+	public void setDatabase(File file) {
+		try {
+			conn = DriverManager.getConnection("jdbc:sqlite:"
+					+ file.getAbsolutePath());
+		} catch (SQLException e) {
+			panic(e, "Unable to load the following file:\n" + file);
 		}
 	}
 

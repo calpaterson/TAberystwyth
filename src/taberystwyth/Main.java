2147957f9aee7518f00ab43b27299d48bcd9816d
Cal Paterson <clp8@aber.ac.uk>
2010-12-04 02:44:53 +0000

    m

--- src/taberystwyth/Main.java
+++ src/taberystwyth/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			Class.forName("taberystwyth.db.SQLConnection");
+			Class.forName("taberystwyth.prelim.Allocator");
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 			Class.forName("taberystwyth.view.OverviewFrame"); //
 		} catch (Exception e){

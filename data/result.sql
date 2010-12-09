562187999b5d604ef57bf228e7a0f88504a6337b
Cal Paterson <clp8@aber.ac.uk>
2010-12-09 16:18:33 +0000

    Added result.sql

---
+++ data/result.sql
@@ -0,0 +1,8 @@
+create table results(
+    "round" smallint not null,
+    "team" integer not null,
+    "position" smallint not null,
+	FOREIGN KEY (team) REFERENCES team(id),
+	PRIMARY KEY (team,round),
+	CONSTRAINT check_position CHECK (position > 0 AND position <5)
+);

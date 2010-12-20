361cc5f341e8ba96ffd7b6c15ab4707b6406efc0
Cal Paterson <clp8@aber.ac.uk>
2010-12-20 20:26:37 +0000

    Licence notice in each file.

--- data/schema.sql
+++ data/schema.sql
@@ -1,4 +1,18 @@
--- Schema.sql
+-- This file is part of TAberystwyth, a debating competition organiser
+-- Copyright (C) 2010, Roberto Sarrionandia and Cal Paterson
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -- Version (used to tell if the current debate is out of date)
 -- This table should have a single entry, which should be the timestamp of the

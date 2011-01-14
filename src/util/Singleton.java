e2ce2b1e29d168eb25222d62517048b9a18fdf8b
tito <r@sarrionandia.com>
2011-01-14 14:22:35 +0000

    Added singleton interface

---
+++ src/util/Singleton.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package util;
+
+/**
+ * @author Cal Paterson
+ * @author Roberto Sarrionandia
+ * 
+ * An interface for the singleton classes in TAberystwyth
+ *
+ */
+public interface Singleton {
+	
+	/**
+	 * Initialise the singleton object
+	 */
+	public void initialise();
+
+}

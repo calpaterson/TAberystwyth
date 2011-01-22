1ab80757159bb66ec2e4a276a24982f5bc387661
Cal Paterson <clp8@aber.ac.uk>
2011-01-22 18:21:09 +0000

    SwingTeamsRequiredException

---
+++ src/taberystwyth/prelim/SwingTeamsRequiredException.java
@@ -0,0 +1,17 @@
+package taberystwyth.prelim;
+
+public class SwingTeamsRequiredException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+    
+    int required;
+    
+    public SwingTeamsRequiredException(int required){
+        this.required = required;
+    }
+
+    public synchronized int getRequired() {
+        return required;
+    }
+    
+}

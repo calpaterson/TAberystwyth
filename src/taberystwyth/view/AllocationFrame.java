aca70b2fe73e4af4efb30f94ab4fdbb9e8733f6f
Roberto Sarrionandia <r@sarrionandia.com>
2011-01-22 17:26:14 +0000

    Added UI elements to allocatoin frame

--- src/taberystwyth/view/AllocationFrame.java
+++ src/taberystwyth/view/AllocationFrame.java
@@ -3,6 +3,8 @@ package taberystwyth.view;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -10,6 +12,13 @@ import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
 
 public class AllocationFrame extends JFrame {
+	
+	JLabel drawTypeLabel = new JLabel("Type of Draw");
+	JComboBox drawTypeBox = new JComboBox();
+	
+	JButton cancel = new JButton("Canel");
+	JButton allocate = new JButton("Allocate");
+	
     
     private static final long serialVersionUID = 1L;
     private static AllocationFrame instance = new AllocationFrame();
@@ -31,8 +40,12 @@ public class AllocationFrame extends JFrame {
                 AllocationFrame.getInstance().setVisible(false);
             }
         });
-        add(new JLabel("Hello!"));
-        add(new JTextField(20));
+        
+        
+        add(drawTypeLabel);
+        add(drawTypeBox);
+        add(cancel);
+        add(allocate);
         pack();
         setMinimumSize(getSize());
     }

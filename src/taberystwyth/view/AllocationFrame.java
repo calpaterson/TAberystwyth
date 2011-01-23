2ecd48f9777702ddb5295751c01ad939c0d3a6de
Roberto Sarrionandia <r@sarrionandia.com>
2011-01-23 19:23:19 +0000

    License, javadoc and code formatting

--- src/taberystwyth/view/AllocationFrame.java
+++ src/taberystwyth/view/AllocationFrame.java
@@ -1,3 +1,20 @@
+/* This file is part of TAberystwyth, a debating competition organiser
+ * Copyright (C) 2010, Roberto Sarrionandia and Cal Paterson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package taberystwyth.view;
 
 import java.awt.event.WindowAdapter;
@@ -9,133 +26,143 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 import taberystwyth.controller.AllocationFrameListener;
 import taberystwyth.prelim.DrawTypeRepository;
 
 import net.miginfocom.swing.MigLayout;
 
+/**
+ * The frame which allows the user to draw a round and select the algorithms he
+ * would like to draw with.
+ * 
+ * @author Roberto Sarrionandia
+ * @author Cal Paterson
+ * 
+ */
 public class AllocationFrame extends JFrame {
-	
+
 	AllocationFrameListener listener = new AllocationFrameListener();
-	
+
 	DrawTypeRepository repos = DrawTypeRepository.getInstance();
-	
+
 	JLabel teamDrawTypeLabel = new JLabel("Draw teams with: ");
-	JTextArea teamDrawTypeDescription = new JTextArea(3,20);
+	JTextArea teamDrawTypeDescription = new JTextArea(3, 20);
 	JComboBox teamDrawTypeBox = new JComboBox();
 	DefaultComboBoxModel teamDrawTypes = new DefaultComboBoxModel();
-	
+
 	JLabel locationDrawTypeLabel = new JLabel("Draw locations with: ");
-	JTextArea locationDrawTypeDescription = new JTextArea(3,20);
+	JTextArea locationDrawTypeDescription = new JTextArea(3, 20);
 	JComboBox locationDrawTypeBox = new JComboBox();
 	DefaultComboBoxModel locationDrawTypes = new DefaultComboBoxModel();
 
 	JLabel judgeDrawTypeLabel = new JLabel("Draw judges with: ");
-	JTextArea judgeDrawTypeDescription = new JTextArea(3,20);
+	JTextArea judgeDrawTypeDescription = new JTextArea(3, 20);
 	JComboBox judgeDrawTypeBox = new JComboBox();
 	DefaultComboBoxModel judgeDrawTypes = new DefaultComboBoxModel();
 
-
 	JButton cancel = new JButton("Cancel");
 	JButton allocate = new JButton("Allocate");
-	
-    
-    private static final long serialVersionUID = 1L;
-    private static AllocationFrame instance = new AllocationFrame();
-    
-    public static AllocationFrame getInstance() {
-        return instance;
-    }
-    
-    private AllocationFrame(){
-        setLayout(new MigLayout("wrap 2, flowx, fillx", "[left]rel[right]"));
-        setTitle("Allocate");
-        
-        /*
-         * When the window is closed, make it invisible
-         */
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent evt) {
-                AllocationFrame.getInstance().setVisible(false);
-            }
-        });
-        
-        
-        /*
-         * Set up description boxes
-         */
-        teamDrawTypeDescription.setLineWrap(true);
-        locationDrawTypeDescription.setLineWrap(true);
-        judgeDrawTypeDescription.setLineWrap(true);
-        
-        teamDrawTypeDescription.setBackground(getBackground());
-        locationDrawTypeDescription.setBackground(getBackground());
-        judgeDrawTypeDescription.setBackground(getBackground());
 
-        
-        /*
-         * Set up comboboxes
-         */      
-        for(String name : repos.getTeamDrawTypeMap().keySet()){        	
-        	teamDrawTypes.addElement(name);
-        }
-        
-        for(String name : repos.getJudgeDrawTypeMap().keySet()){        	
-        	judgeDrawTypes.addElement(name);
-        }
+	private static final long serialVersionUID = 1L;
+	private static AllocationFrame instance = new AllocationFrame();
 
-        for(String name : repos.getLocationDrawTypeMap().keySet()){        	
-        	locationDrawTypes.addElement(name);
-        }
-        
-        teamDrawTypeBox.setModel(teamDrawTypes);
-        judgeDrawTypeBox.setModel(judgeDrawTypes);
-        locationDrawTypeBox.setModel(locationDrawTypes);
-        
-        /*
-         * Add to the frame
-         */
-        add(teamDrawTypeLabel);
-        add(teamDrawTypeBox);
-        add(teamDrawTypeDescription, "wrap");
-        
-        add(judgeDrawTypeLabel);
-        add(judgeDrawTypeBox);
-        add(judgeDrawTypeDescription, "wrap");
+	/**
+	 * Return the instance of the frame
+	 * 
+	 * @return The AllocationFrame instance
+	 */
+	public static AllocationFrame getInstance() {
+		return instance;
+	}
 
-        add(locationDrawTypeLabel);
-        add(locationDrawTypeBox);
-        add(locationDrawTypeDescription, "wrap");
-        
-        add(cancel, "tag cancel");
-        add(allocate, "tag apply");
-        pack();
-        setMinimumSize(getSize());
-        
-        /*
-         * Set up listener
-         */
-        cancel.addActionListener(listener);
-        allocate.addActionListener(listener);
-        
-        teamDrawTypeBox.addItemListener(listener);
-        judgeDrawTypeBox.addItemListener(listener);
-        locationDrawTypeBox.addItemListener(listener);
-        
-        updateDescriptions();
-    }
-    
-    public void updateDescriptions(){
-    	teamDrawTypeDescription.setText(
-    			repos.getTeamDrawTypeMap().get(teamDrawTypeBox.getSelectedItem()));
-    	judgeDrawTypeDescription.setText(
-    			repos.getJudgeDrawTypeMap().get(judgeDrawTypeBox.getSelectedItem()));
-    	locationDrawTypeDescription.setText(
-    			repos.getLocationDrawTypeMap().get(locationDrawTypeBox.getSelectedItem()));
-    	
-    }
-    
+	private AllocationFrame() {
+		setLayout(new MigLayout("wrap 2, flowx, fillx", "[left]rel[right]"));
+		setTitle("Allocate");
+
+		/*
+		 * When the window is closed, make it invisible
+		 */
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent evt) {
+				AllocationFrame.getInstance().setVisible(false);
+			}
+		});
+
+		/*
+		 * Set up description boxes
+		 */
+		teamDrawTypeDescription.setLineWrap(true);
+		locationDrawTypeDescription.setLineWrap(true);
+		judgeDrawTypeDescription.setLineWrap(true);
+
+		teamDrawTypeDescription.setBackground(getBackground());
+		locationDrawTypeDescription.setBackground(getBackground());
+		judgeDrawTypeDescription.setBackground(getBackground());
+
+		/*
+		 * Set up comboboxes
+		 */
+		for (String name : repos.getTeamDrawTypeMap().keySet()) {
+			teamDrawTypes.addElement(name);
+		}
+
+		for (String name : repos.getJudgeDrawTypeMap().keySet()) {
+			judgeDrawTypes.addElement(name);
+		}
+
+		for (String name : repos.getLocationDrawTypeMap().keySet()) {
+			locationDrawTypes.addElement(name);
+		}
+
+		teamDrawTypeBox.setModel(teamDrawTypes);
+		judgeDrawTypeBox.setModel(judgeDrawTypes);
+		locationDrawTypeBox.setModel(locationDrawTypes);
+
+		/*
+		 * Add to the frame
+		 */
+		add(teamDrawTypeLabel);
+		add(teamDrawTypeBox);
+		add(teamDrawTypeDescription, "wrap");
+
+		add(judgeDrawTypeLabel);
+		add(judgeDrawTypeBox);
+		add(judgeDrawTypeDescription, "wrap");
+
+		add(locationDrawTypeLabel);
+		add(locationDrawTypeBox);
+		add(locationDrawTypeDescription, "wrap");
+
+		add(cancel, "tag cancel");
+		add(allocate, "tag apply");
+		pack();
+		setMinimumSize(getSize());
+
+		/*
+		 * Set up listener
+		 */
+		cancel.addActionListener(listener);
+		allocate.addActionListener(listener);
+
+		teamDrawTypeBox.addItemListener(listener);
+		judgeDrawTypeBox.addItemListener(listener);
+		locationDrawTypeBox.addItemListener(listener);
+
+		updateDescriptions();
+	}
+
+	/**
+	 * Force the frame to update the text areas that contain the descriptions of
+	 * the algorithms selected
+	 */
+	public void updateDescriptions() {
+		teamDrawTypeDescription.setText(repos.getTeamDrawTypeMap().get(
+				teamDrawTypeBox.getSelectedItem()));
+		judgeDrawTypeDescription.setText(repos.getJudgeDrawTypeMap().get(
+				judgeDrawTypeBox.getSelectedItem()));
+		locationDrawTypeDescription.setText(repos.getLocationDrawTypeMap().get(
+				locationDrawTypeBox.getSelectedItem()));
+	}
 }

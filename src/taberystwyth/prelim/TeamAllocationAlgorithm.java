337a34f98032a48ad5628ab9102c1b76b92def22
Cal Paterson <clp8@aber.ac.uk>
2011-01-24 14:12:19 +0000

    Lots of team allocation written

--- src/taberystwyth/prelim/TeamAllocationAlgorithm.java
+++ src/taberystwyth/prelim/TeamAllocationAlgorithm.java
@@ -3,18 +3,50 @@
  */
 package taberystwyth.prelim;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import taberystwyth.db.SQLConnection;
+
 /**
  * @author Roberto Sarrionandia [r@sarrionandia.com]
  * 
- * The abstract class for team allocation algorithms.
- * Algorithms should inherit this class.
- *
+ *         The abstract class for team allocation algorithms. Algorithms should
+ *         inherit this class.
+ * 
  */
 public abstract class TeamAllocationAlgorithm extends AllocationAlgorithm {
-	@Override
-    public void allocate() {
-		// TODO Auto-generated method stub
-		
-	}
-	
+    public ArrayList<String> getTeamNames() throws SQLException {
+        ArrayList<String> teamNames = new ArrayList<String>();
+        String query = "select name from teams;";
+        ResultSet rs = SQLConnection.getInstance().executeQuery(query);
+        while (rs.next()) {
+            teamNames.add(rs.getString("name"));
+        }
+        return teamNames;
+    }
+    
+    public HashMap<String, Integer> getTeamPoints() throws SQLException{
+        HashMap<String,Integer> teamScores = new HashMap<String,Integer>();
+        for (String name:getTeamNames()){
+            int teamPoints = 0;
+            String query = "select position from team_results where team = '" + name + "';";
+            ResultSet rs = SQLConnection.getInstance().executeQuery(query);
+            while(rs.next()){
+                int position = rs.getInt("position");
+                if (position == 1){
+                    teamPoints += 3;
+                } else if (position == 2){
+                    teamPoints += 2;
+                } else if (position == 3){
+                    teamPoints += 1;
+                } else {
+                    // void
+                }
+            }
+        }
+        return teamScores;
+    }
 }
---
+++ src/taberystwyth/prelim/WUDCAllocationAlgorithm.java
@@ -0,0 +1,92 @@
+package taberystwyth.prelim;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.TreeMap;
+
+public class WUDCAllocationAlgorithm extends TeamAllocationAlgorithm {
+    
+    Random randomGenerator = new Random(0L);
+    
+    @Override
+    void allocate() throws SQLException {
+        
+    }
+    
+    private TreeMap<Integer, ArrayList<String>> getLeveledPools()
+        throws SQLException{
+        TreeMap<Integer, ArrayList<String>> pools = 
+            new TreeMap<Integer, ArrayList<String>>();
+        
+        /*
+         * For each pool, starting at the top (highest ranked pool) and 
+         * working down...
+         */
+        for (int i = pools.lastKey(); i > 0; --i){
+            /*
+             * if the pool exists...
+             */
+            if (pools.containsKey(i)){
+                /*
+                 * and the pools size is not a multple of 4...
+                 */
+                while (!((pools.get(i).size() % 4) == 0)){
+                    /*
+                     * then pull up (randomly) a member from the pool directly
+                     * below this one
+                     */
+                    for (int j = (i - 1); i > 0; --j){
+                        if (pools.containsKey(j)){
+                            ArrayList<String> lowerPool = pools.get(j);
+                            int randomElementIndex = 
+                                randomGenerator.nextInt(lowerPool.size());
+                            pools.get(i).add(pools.get(j).remove(randomElementIndex));
+                            
+                            /*
+                             * If the arraylist is empty then delete it
+                             */
+                            if (lowerPool.size() == 0){
+                                pools.remove(j);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return pools;
+    }
+    
+    private TreeMap<Integer, ArrayList<String>> getPools() throws SQLException{
+        /*
+         * Construct the map of points to teams (pools)
+         */
+        TreeMap<Integer, ArrayList<String>> pools = 
+            new TreeMap<Integer, ArrayList<String>>();
+        
+        /*
+         * Get the map of team names to points
+         */
+        HashMap<String, Integer> points = getTeamPoints();
+        
+        /*
+         * For each team, add it to map of pools
+         */
+        for (String team: points.keySet()){
+           int innerPoints = points.get(team);
+           /*
+            * If the pool does not exist, create it
+            */
+           if (!pools.containsKey(innerPoints)){
+               pools.put(innerPoints, new ArrayList<String>());
+           }
+           pools.get(innerPoints).add(team);
+        }
+        
+        return pools;
+    }
+    
+}

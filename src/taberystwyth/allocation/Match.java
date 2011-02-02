ba2ee67c14fa01ee3f77dc9596461546303a881b
Cal Paterson <clp8@aber.ac.uk>
2011-02-02 18:42:50 +0000

    JAutodoc produces surprisingly good documentation

--- src/taberystwyth/allocation/Match.java
+++ src/taberystwyth/allocation/Match.java
@@ -19,66 +19,194 @@ package taberystwyth.allocation;
 
 import java.util.ArrayList;
 
+/**
+ * The Match class represents what is currently referred to as a "room" in the
+ * schema.
+ */
 public class Match {
+    
+    /** The first prop. */
     private String firstProp;
+    
+    /** The second prop. */
     private String secondProp;
+    
+    /** The first op. */
     private String firstOp;
+    
+    /** The second op. */
     private String secondOp;
+    
+    /** The chair. */
     private String chair;
+    
+    /** The wings. */
     private ArrayList<String> wings = new ArrayList<String>();
+    
+    /** The location. */
     private String location;
+    
+    /** The rank. */
     private int rank;
+    
+    /**
+     * Instantiates a new match.
+     *
+     * @param rank the rank
+     */
     public Match(int rank) {
         this.rank = rank;
     }
+    
+    /**
+     * Gets the first prop.
+     *
+     * @return the first prop
+     */
     public synchronized String getFirstProp() {
         return firstProp;
     }
+    
+    /**
+     * Sets the first prop.
+     *
+     * @param firstProp the new first prop
+     */
     public synchronized void setFirstProp(String firstProp) {
         this.firstProp = firstProp;
     }
+    
+    /**
+     * Gets the second prop.
+     *
+     * @return the second prop
+     */
     public synchronized String getSecondProp() {
         return secondProp;
     }
+    
+    /**
+     * Sets the second prop.
+     *
+     * @param secondProp the new second prop
+     */
     public synchronized void setSecondProp(String secondProp) {
         this.secondProp = secondProp;
     }
+    
+    /**
+     * Gets the first op.
+     *
+     * @return the first op
+     */
     public synchronized String getFirstOp() {
         return firstOp;
     }
+    
+    /**
+     * Sets the first op.
+     *
+     * @param firstOp the new first op
+     */
     public synchronized void setFirstOp(String firstOp) {
         this.firstOp = firstOp;
     }
+    
+    /**
+     * Gets the second op.
+     *
+     * @return the second op
+     */
     public synchronized String getSecondOp() {
         return secondOp;
     }
+    
+    /**
+     * Sets the second op.
+     *
+     * @param secondOp the new second op
+     */
     public synchronized void setSecondOp(String secondOp) {
         this.secondOp = secondOp;
     }
+    
+    /**
+     * Gets the chair.
+     *
+     * @return the chair
+     */
     public synchronized String getChair() {
         return chair;
     }
+    
+    /**
+     * Sets the chair.
+     *
+     * @param chair the new chair
+     */
     public synchronized void setChair(String chair) {
         this.chair = chair;
     }
+    
+    /**
+     * Gets the wings.
+     *
+     * @return the wings
+     */
     public synchronized ArrayList<String> getWings() {
         return wings;
     }
+    
+    /**
+     * Gets the location.
+     *
+     * @return the location
+     */
     public synchronized String getLocation() {
         return location;
     }
+    
+    /**
+     * Sets the location.
+     *
+     * @param location the new location
+     */
     public synchronized void setLocation(String location) {
         this.location = location;
     }
+    
+    /**
+     * Gets the rank.
+     *
+     * @return the rank
+     */
     public synchronized int getRank() {
         return rank;
     }
+    
+    /**
+     * Sets the rank.
+     *
+     * @param rank the new rank
+     */
     public synchronized void setRank(int rank) {
         this.rank = rank;
     }
+    
+    /**
+     * Checks whether a chair has been set.
+     *
+     * @return true, if successful
+     */
     public synchronized boolean hasChair(){
         return chair != null;
     }
+    
+    /**
+     * Adds a wing.
+     *
+     * @param name the name
+     */
     public void addWing(String name) {
         wings.add(name);        
     }

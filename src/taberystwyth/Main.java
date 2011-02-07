dee1cc8589c3304b6ca2b8b34c33d928e6039537
Cal Paterson <clp8@aber.ac.uk>
2011-02-07 13:07:50 +0100

    Logging Main.class

--- src/taberystwyth/Main.java
+++ src/taberystwyth/Main.java
@@ -20,42 +20,70 @@ package taberystwyth;
 
 import javax.swing.*;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 import taberystwyth.controller.DebugMenuListener;
 import taberystwyth.view.OverviewFrame;
 
 /**
  * The Main class
+ * 
  * @author Roberto Sarrionandia and Cal Paterson
  */
 public class Main {
     
+    static Logger logger = Logger.getLogger(Main.class);
+    
     /**
      * The main method
-     * @param args arguments passed to the program
+     * 
+     * @param args
+     *            arguments passed to the program
      */
     public static void main(String[] args) {
+        /*
+         * Set up logging
+         */
+        BasicConfigurator.configure();
+        
+        /*
+         * Try setting the look and feel. If it doesn't work, we just fail and
+         * continue.
+         */
         try {
             UIManager
                     .setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e1) {
+            logger.error(
+                    "Unable to set the system look and feel, will carry on with the default.",
+                    e1);
+            e1.printStackTrace();
+        }
+        
+        try {
+            
             Class.forName("taberystwyth.db.SQLConnection");
             Class.forName("taberystwyth.allocation.Allocator");
             Class.forName("taberystwyth.view.OverviewFrame");
         } catch (Exception e) {
+            logger.fatal("Unable to load the singleton classes.", e);
             e.printStackTrace();
+            return;
         }
         
         /*
-         * If the debug argument is passed, add the debug menu
-         * FIXME: Clean this up so that the overviewframe menu has an option
-         * to display the debug menu or not - this stuff shouldn't be here.
+         * If the debug argument is passed, add the debug menu FIXME: Clean
+         * this up so that the overviewframe menu has an option to display the
+         * debug menu or not - this stuff shouldn't be here.
          */
         if (args.length > 0) {
             if (args[0].equals("--debug")) {
-                System.out.println("Entering Debug Mode");
+                logger.info("Entering debug mode.");
                 
                 JMenu debugMenu = new JMenu("Debug");
-                JMenuItem generateMorningTab = 
-                    new JMenuItem("Generate Morning Tab");
+                JMenuItem generateMorningTab = new JMenuItem(
+                        "Generate Morning Tab");
                 generateMorningTab.addActionListener(new DebugMenuListener());
                 debugMenu.add(generateMorningTab);
                 

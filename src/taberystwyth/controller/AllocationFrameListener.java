e4d509df294878b17792711066aca8442e589380
Cal Paterson <clp8@aber.ac.uk>
2011-02-14 23:49:09 +0000

    Allocation seems to work

--- src/taberystwyth/controller/AllocationFrameListener.java
+++ src/taberystwyth/controller/AllocationFrameListener.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
+import org.apache.log4j.Logger;
+
 import taberystwyth.allocation.Allocator;
 import taberystwyth.allocation.exceptions.JudgesRequiredException;
 import taberystwyth.allocation.exceptions.LocationsRequiredException;
@@ -20,6 +22,8 @@ import taberystwyth.view.AllocationFrame;
 
 public class AllocationFrameListener implements ActionListener, ItemListener {
     
+    private static final Logger LOG = Logger.getLogger(AllocationFrame.class);
+    
     @Override
     public void actionPerformed(ActionEvent event) {
         
@@ -28,20 +32,18 @@ public class AllocationFrameListener implements ActionListener, ItemListener {
         }
 
         else if (event.getActionCommand().equals("Allocate")) {
-            try {
-                AllocationFrame frame = AllocationFrame.getInstance();
-                
+            try {                
                 Allocator.getInstance().allocate(new WUDC(), new Balanced(), new Random());
-                
             } catch (SQLException e) {
                 /*
                  * This is a "proper" error...not much can be done
                  */
-                e.printStackTrace();
+                LOG.error("Allocation caused an sql exception", e);
                 JOptionPane.showMessageDialog(AllocationFrame.getInstance(),
                                 "Allocation has thrown an SQL Error.",
                                 "SQL Error", JOptionPane.ERROR_MESSAGE);
             } catch (SwingTeamsRequiredException e) {
+                LOG.error("Need swing teams", e);
                 JOptionPane.showMessageDialog(
                                 AllocationFrame.getInstance(),
                                 "The total number of teams needs to be divisble by "
@@ -51,6 +53,7 @@ public class AllocationFrameListener implements ActionListener, ItemListener {
                                 "Swing Teams Required",
                                 JOptionPane.INFORMATION_MESSAGE);
             } catch (LocationsRequiredException e) {
+                LOG.error("Need more locations", e);
                 JOptionPane.showMessageDialog(
                                 AllocationFrame.getInstance(),
                                 "Not enough locations in order to run this round.  "
@@ -60,6 +63,7 @@ public class AllocationFrameListener implements ActionListener, ItemListener {
                                 "Not enough locations",
                                 JOptionPane.INFORMATION_MESSAGE);
             } catch (JudgesRequiredException e) {
+                LOG.error("Need more judges", e);
                 JOptionPane.showMessageDialog(
                                 AllocationFrame.getInstance(),
                                 "Not enough judges in order to run this round.  "

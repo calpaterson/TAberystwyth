f3666efa5cab9aa527f33eda904a8e9411aacc8c
Cal Paterson <clp8@aber.ac.uk>
2011-02-28 18:18:33 +0000

    Added index functionality to DataDirectory

--- src/taberystwyth/utils/DataDirectory.java
+++ src/taberystwyth/utils/DataDirectory.java
@@ -1,14 +1,20 @@
 package taberystwyth.utils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 
 public class DataDirectory {
     
-    private static final Logger LOG = Logger
-                    .getLogger(DataDirectory.class);
+    private static final Logger LOG = Logger.getLogger(DataDirectory.class);
     
     private static final DataDirectory INSTANCE = new DataDirectory();
     
@@ -20,12 +26,50 @@ public class DataDirectory {
         /* VOID */
     }
     
+    File index = new File(getDirectory().getAbsolutePath()
+                    + System.getProperty("file.separator") + "index");
+    
+    public HashMap<String, File> getIndex() {
+        HashMap<String, File> returnValue = null;
+        try {
+            FileInputStream fis = new FileInputStream(index);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            returnValue = (HashMap<String, File>) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (Exception e){
+            LOG.fatal("Unable to read index file", e);
+            System.exit(1);
+        }
+        return returnValue;
+    }
+    
+    public void setIndex(HashMap<String, File> map) {
+        try {
+            /*
+             * Delete and recreate the index file
+             */
+            if (index.exists()) {
+                index.delete();
+            }
+            index.createNewFile();
+            
+            FileOutputStream fos = new FileOutputStream(index);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(map);
+            fos.close();
+            oos.close();
+        } catch (IOException e) {
+            LOG.fatal("Was unable to write to the index file", e);
+            System.exit(1);
+        }
+    }
+    
     public File getDirectory() {
-        // FIXME: Statically hacked
-        String directoryPath = System.getProperty("user.home") +
-                               System.getProperty("file.separator") +
-                               ".taberystwyth" + 
-                               System.getProperty("file.separator");
+        String directoryPath = System.getProperty("user.home")
+                        + System.getProperty("file.separator")
+                        + ".taberystwyth"
+                        + System.getProperty("file.separator");
         LOG.info("Directory set as: " + directoryPath);
         File directory = new File(directoryPath);
         

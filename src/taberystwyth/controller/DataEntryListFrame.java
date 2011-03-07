0444742465fa3503a52a9e224fa848d07cb9ed7f
Roberto Sarrionandia <r@sarrionandia.com>
2011-03-07 19:04:38 +0000

    New frame for choosing a room, but it doesnt work yet

---
+++ src/taberystwyth/controller/DataEntryListFrame.java
@@ -0,0 +1,12 @@
+package taberystwyth.controller;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Roberto Sarrionandia [r@sarrionandia.com]
+ *  A frame to show a list of rooms that the user can select from 
+ *  to show the data entry frame for that particular room
+ */
+public class DataEntryListFrame extends JFrame {
+    
+}
